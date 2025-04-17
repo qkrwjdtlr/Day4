92ba868565b602dfc5460ddfa6ef2e98b9224e0b
qkrwjdtlr <qkrwjdtlrz@naver.com>
2025-04-17 16:30:08 +0900

    t

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Day4
 {
-    internal class Program
+    
+    class Program
     {
         static void Main()
         {
+
+
+
+            Console.WriteLine("");
+
+
 
         }
     }
