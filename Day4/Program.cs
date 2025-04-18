63a5a7c21b7bf5a94632c224f60cb4d877ce463c
qkrwjdtlr <qkrwjdtlrz@naver.com>
2025-04-18 12:16:16 +0900

    t

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,22 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 
+class Item
+{
+    //get 으로 읽고 set 값을 수정한다  
+    public string Name { get; set; }
+
+    public int Count { get; set; }
+
+
+    public Item(string name, int count)
+    {
+        Name = name;
+        Count = count;
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
 class RPGGame
 {
-
     static void Main()
     {
-        Console.WriteLine("ok!!!");
-        Console.WriteLine("ok_2");
-
-
-
-
-
-
-
-
-
         Console.ReadLine();
     }
 }
